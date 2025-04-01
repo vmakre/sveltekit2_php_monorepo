39dee9362f173e82f49cdd67b96c6c17f47b08d4
Vladimir Makreski <vmakre@gmail.com>
2025-04-01 19:23:25 +0200

    Delete vendor directory

--- vendor/mevdschee/php-crud-api/src/Tqdev/PhpCrudApi/Cache/NoCache.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tqdev\PhpCrudApi\Cache;
-
-use Tqdev\PhpCrudApi\Cache\Base\BaseCache;
-
-class NoCache extends BaseCache implements Cache
-{
-}
--- vendor/mevdschee/php-crud-api/src/Tqdev/PhpCrudApi/Config/Base/ConfigInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tqdev\PhpCrudApi\Config\Base;
-
-interface ConfigInterface
-{
-    public function getMiddlewares();
-    public function getProperty(string $key, $default = '');
-}
