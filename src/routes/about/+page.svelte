767a513d9c17e34d8f7e135e4f4a6419801d4465
Vladimir Makreski <vmakre@gmail.com>
2025-04-22 09:44:36 +0200

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -19,7 +19,7 @@
 
 
 </script>
-<h1>ABOUT SvelteKit AAAAAA</h1>
+<h1>ABOUT SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 {#if data}
